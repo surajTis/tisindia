5ad483d9fb1ac7533f396b50f0efa626aeeb6be8
Rupali <rupali@tisindia.com>
2023-10-05 00:42:49 -0700

    design homeAbout

--- components/Page/Page.js
+++ components/Page/Page.js
@@ -4,6 +4,7 @@ import Head from "next/head";
 import { MainMenu } from "components/MainMenu";
 import { Footer } from "components/Footer";
 import { BlogInsight } from "components/BlogInsight";
+import { HomeAbout } from "components/HomeAbout/HomeAbout";
 export const Page = (props) => {
   console.log("PAGE PROPS: ", props);
   return (
@@ -23,8 +24,10 @@ export const Page = (props) => {
         callToActionDestination={props.callToActionDestination}
         callToActionLabel={props.callToActionLabel}
       />
-      <BlogInsight />
+      
       <BlockRenderer blocks={props.blockContent} />
+      <HomeAbout />
+      <BlogInsight />
       <Footer items={props.footerMenuItems}/>
     </PageWrapper>
   );
