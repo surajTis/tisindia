700f675c29297352ea599c98e8a4dcfcaf6f3148
surajTis <146411391+surajTis@users.noreply.github.com>
2023-11-28 12:29:13 +0530

    Update MainMenu.js

--- components/MainMenu/MainMenu.js
+++ components/MainMenu/MainMenu.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 import React, { useState, useRef, useEffect } from "react";
-=======
->>>>>>> b9249c0cd5494e10ce5549c6266f4ee439a37b2d
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "../../public/assets/img/logo/sticky-logo-white-01.svg";
@@ -17,28 +14,9 @@ export const MainMenu = ({
 }) => {
   const { sticky } = useSticky();
 
-<<<<<<< HEAD
-  // const [open, setOpen] = useState(false);
-
-  //   const ToggleSidebar = () => {
-  //     open === true ? setOpen(false) : setOpen(true);
-  //   }
-  //   const ref = useRef();
-
-  //   useEffect(() => {
-  //     const handleClickOutside = (event) => {
-  //       if (!ref.current.contains(event.target)) {
-  //         setOpen(false);
-  //       }
-  //     };
-  //     document.addEventListener("mousedown", handleClickOutside);
-  //   }, [ref]);
 
   const [isActive, setIsActive] = useState(false);
-   
-  
-=======
->>>>>>> b9249c0cd5494e10ce5549c6266f4ee439a37b2d
+
   return (
     <>
       <header className={`header ${sticky ? "header-sticky" : ""}`}>
