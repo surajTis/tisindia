9da524f7c71f7e9a6a1bea4c2d59325d28373763
unknown <suraj@tisindia.com>
2023-12-08 15:00:01 +0530

    demo 3

--- postcss.config.js
+++ postcss.config.js
@@ -17,7 +17,9 @@ module.exports = {
         '@fullhuman/postcss-purgecss',
         {
           content: [
-              './components/**/*.{js,jsx,ts,tsx}'
+              './components/**/*.{js,jsx,ts,tsx}',
+              './common/*.{js,jsx,ts,tsx}',
+              './common/layout/*.{js,jsx,ts,tsx}'
           ],
           defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
           safelist: ["html", "body"]
